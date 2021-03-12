676d00bb7b2fcffada12b092e2beb8a93ee83312
Bugra <abugraokkali@outlook.com>
2021-03-13 00:23:36 +0300

    Getting ready for deployment to Heroku

--- app/src/main/java/appintro/App.java
+++ app/src/main/java/appintro/App.java
@@ -4,6 +4,14 @@
 package appintro;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 
 public class App {
     public String getGreeting() {
@@ -11,7 +19,50 @@ public class App {
     }
 
     public static void main(String[] args) {
+        StackTraceElement[] stack = Thread.currentThread ().getStackTrace ();
+        StackTraceElement main = stack[stack.length - 1];
+        String mainClass = main.getClassName ();
+        System.out.println(mainClass);
         System.out.println(new App().getGreeting());
+        
+        get("/",(rq,rs)-> "Hello World");
+        get("/compute",
+        (rq,rs)-> {
+            Map<String,String>map= new HashMap<String,String>();
+            map.put("result","not computed yet");
+            return new ModelAndView(map,"compute.mustache");
+        },
+        new MustacheTemplateEngine()
+        );
+
+        post("/compute",
+        (req,rs)->{
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+            String input1 =req.queryParams("input1");
+            java.util.Scanner sc1=new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer>inputList=new java.util.ArrayList<>();
+            while(sc1.hasNext()){
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList.add(value);
+            }
+            sc1.close();
+            System.out.println(inputList);
+
+            String input2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(input2);
+
+            boolean result = App.search(inputList, input2AsInt);
+
+            Map<String,Boolean>map=new HashMap<>();
+            map.put("result",result);
+            return new ModelAndView(map,"compute.mustache");
+        },
+        new MustacheTemplateEngine()
+        );
+    
+    
     }
     public static boolean search(ArrayList<Integer> arr,int e){
         if(arr==null)return false;
